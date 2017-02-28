8e91f23c68bf0e3332816b43d0fbd2020d9d6635
Ducoudray <ducoudray.alex@gmail.com>
2017-02-28 15:26:26 +0100

    Javadoc !

--- src/commande/ListeTrieeCirculaireDeDemandes.java
+++ src/commande/ListeTrieeCirculaireDeDemandes.java
@@ -3,7 +3,13 @@ package commande;
 
 import outils.Demande;
 import outils.Sens;
-
+/**
+ * Classe implémentant l'interface <code>IListeTrieeCirculaire</code>, elle représente la liste des demandes de l'ascenseur et est capable de trier cette liste en fonction des sens de navigation et des étages demandés.
+ * @author Léo, Vincent
+ * @version 2.0
+ * @see Demande
+ * @see Sens
+ */
 public class ListeTrieeCirculaireDeDemandes implements IListeTrieeCirculaire<Demande> 
 {
 	private int taille_liste = 0;
@@ -11,6 +17,10 @@ public class ListeTrieeCirculaireDeDemandes implements IListeTrieeCirculaire<Dem
 	public boolean[] descente;
 	public Demande[] listeTrieeFinale; // tableau final de demandes triées
 	
+	/**
+	 * Constructeur, créée 3 listes : une liste des demandes en montée, une liste des demandes en descente et une liste finale
+	 * @param taille_liste <code>int</code> : taille des listes de montée et de descente.
+	 */
 	public ListeTrieeCirculaireDeDemandes(int taille_liste) 
 	{
 		this.montee = new boolean[taille_liste];
@@ -24,6 +34,10 @@ public class ListeTrieeCirculaireDeDemandes implements IListeTrieeCirculaire<Dem
 	
 	
 	@Override
+	/**
+	 * Retourne le nombre de demande dans la liste triée
+	 * @return <code>int</code> la vrai taille
+	 */
 	public int taille() 
 	{
 		int vrai_taille =0;
@@ -38,17 +52,28 @@ public class ListeTrieeCirculaireDeDemandes implements IListeTrieeCirculaire<Dem
 	}
 
 	@Override
+	/**
+	 * Retourne si la liste est vide ou non
+	 * @return <code>boolean</code> <code>true</code> si la liste est vide, <code>false</code> sinon.
+	 */
 	public boolean estVide() 
 	{
 		return (listeTrieeFinale[0] == null);
 	}
 
+	/**
+	 * Retourne si la liste est pleine ou non
+	 * @return <code>boolean</code> <code>true</code> si la liste est pleine <code>false</code> sinon
+	 */
 	public boolean estPleine() 
 	{
 		return (listeTrieeFinale[listeTrieeFinale.length-1] != null);
 	}
 	
 	@Override
+	/**
+	 * Vide la liste de toutes ses demandes
+	 */
 	public void vider() 
 	{
 		for(int i =0;i<listeTrieeFinale.length;i++)
@@ -58,26 +83,29 @@ public class ListeTrieeCirculaireDeDemandes implements IListeTrieeCirculaire<Dem
 	}
 
 	@Override
+	/**
+	 * Retourne si la liste contient la demande spécifiée en paramètre
+	 * @param d <code>Demande</code> la demande à trouver dans la liste
+	 * @return <code>boolean</code> <code>true</code> si la liste contient la demande <code>false</code> sinon
+	 */
 	public boolean contient(Demande d) 
 	{
-		int i = 0;
-		/*
-		 * On crée une boucle qui s'arrête dès lors que l'on trouve une correspondance.
-		 * A défaut la boucle renvoie faux.
-		 * */
-		while(i<listeTrieeFinale.length)
+		for(int i=0; i<taille(); i++)
 		{
-			if(listeTrieeFinale[i] == d)
+			if(listeTrieeFinale[i].equals(d))
 			{
 				return true;
 			}
-			i++;
 		}
 		return false;
-		
 	}
 
 	@Override
+	/**
+	 * Insère la demande spécifiée en paramètre dans la liste
+	 * @param <code>Demande</code> demande à insérer
+	 * @throws <code>IllegalArgumentException</code>
+	 */
 	public void inserer(Demande d) throws IllegalArgumentException
 	{
 		if(!estPleine() && !contient(d))
@@ -123,37 +151,37 @@ public class ListeTrieeCirculaireDeDemandes implements IListeTrieeCirculaire<Dem
 	}
 
 	@Override
+	/**
+	 * Supprime la demande spécifiée en paramètre
+	 * @param <code>Demande</code> la demande à supprimer
+	 * @throws <code>IllegalArgumentException</code> si la demande n'existe pas dans la liste ou si le sens de la demande est <code>INDEFINI</code>
+	 */
 	public void supprimer(Demande d) throws IllegalArgumentException
 	{	
-		if(!estVide() && contient(d))
+		if(contient(d))
 		{
 			if(d.sens() == Sens.MONTEE)
 			{
-				for(int i=0;i<montee.length;i++)
-				{
-					if(d.etage() == i)
-					{
-						montee[i]=false;
-					}
-				}
+				montee[d.etage()]=false;
 			}
 			
 			else if(d.sens() == Sens.DESCENTE)
 			{
-				for(int i=0;i<descente.length;i++)
-				{
-					if(d.etage() == i)
-					{
-						descente[i]=false;
-					}	
-				}	
+				descente[d.etage()]=false;
 			}
 			else throw new IllegalArgumentException();
-			triTabFinal();
 		}
+		else throw new IllegalArgumentException();
+		triTabFinal();
+
 	}
 
 	@Override
+	/**
+	 * Renvoie la demande suivante de la demande spécifiée en paramètre
+	 * @return <code>Demande</code> la demande suivante
+	 * @param <code>Demande</code> la demande courante
+	 */
 	public Demande suivantDe(Demande courant) 
 	{
 		int bonneDemande = 0;
@@ -178,12 +206,15 @@ public class ListeTrieeCirculaireDeDemandes implements IListeTrieeCirculaire<Dem
 						flag= true;
 					}
 				}
-				
 			}	
 		}
 		return listeTrieeFinale[bonneDemande];
 	}
 	
+	/**
+	 * Renvoie une chaîne de caractère représentant le contenu de la liste des demandes
+	 * @return <code>String</code>
+	 */
 	public String toString()
 	{
 		String phraseFinale = "";
@@ -211,6 +242,9 @@ public class ListeTrieeCirculaireDeDemandes implements IListeTrieeCirculaire<Dem
 		return phraseFinale;
 	}
 	
+	/**
+	 * Trie la liste des demande en fonction des etages et des sens des demandes
+	 */
 	private void triTabFinal()
 	{
 		vider();
